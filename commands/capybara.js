cac4766277e23341b122111caca5f72d5208cb34
Dudus Jestem <dudak799@gmail.com>
2021-12-13 12:08:33 +0100

    Update capybara.js

--- commands/capybara.js
+++ commands/capybara.js
@@ -7,7 +7,7 @@ var Scraper = require('images-scraper');
 const google = new Scraper({
 	userAgent: 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0',
 	puppeteer: {
-		//headless: false,
+		headless: true,
 	},
 });
 
